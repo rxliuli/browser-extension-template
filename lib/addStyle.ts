59cf9c5b8cb8832495396bbce811180ce52738bd
rxliuli <rxliuli@gmail.com>
2025-11-08 22:22:51 +0800

    feat: update add style

--- lib/addStyle.ts
+++ lib/addStyle.ts
@@ -1,8 +1,9 @@
 export function addStyle(shadow: ShadowRoot, styles: string[]) {
-  const sheets = styles.map((style) => {
-    const sheet = new CSSStyleSheet()
-    sheet.replaceSync(style.replaceAll(':root', ':host'))
-    return sheet
-  })
-  shadow.adoptedStyleSheets = sheets
+  const style = document.createElement('style')
+  style.textContent = styles.join('\n').replaceAll(':root', ':host')
+  if (shadow.firstChild) {
+    shadow.insertBefore(style, shadow.firstChild)
+  } else {
+    shadow.appendChild(style)
+  }
 }
